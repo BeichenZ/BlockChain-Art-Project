940a658f633910497106d15207f1333501062882
Howard-Zhou <howard12345678987654321@hotmail.com>
2018-03-10 20:21:15 -0800

    Merge map when exploring

--- shared/robot.go
+++ shared/robot.go
@@ -1,7 +1,9 @@
 package shared
 
 import (
+	"math/rand"
 	"net/rpc"
+	"time"
 )
 
 type RobotStruct struct {
@@ -11,17 +13,19 @@ type RobotStruct struct {
 	RMap            Map
 	CurPath         Path
 	CurLocation     PointStruct
+	NextStep        Coordinate
 	JoiningSig      chan bool
 	BusySig         chan bool
 	WaitingSig      chan bool
 	FreeSpaceSig    chan bool
 	WallSig         chan bool
+	WalkSig         chan bool
 }
 
 type Robot interface {
 	SendMyMap(rID uint, rMap Map)
 	MergeMaps(neighbourMaps []Map) error
-	Explore() error//make a step base on the robat's current path
+	Explore() error //make a step base on the robat's current path
 	GetMap() Map
 }
 
@@ -33,6 +37,7 @@ func (r *RobotStruct) SendMyMap(rId uint, rMap Map) {
 
 func (r *RobotStruct) Explore() error {
 	for {
+		time.Sleep(time.Millisecond * time.Duration(1000))
 		select {
 		case <-r.JoiningSig:
 			// TODO do joining thing
@@ -45,10 +50,49 @@ func (r *RobotStruct) Explore() error {
 			select {
 			case <-r.FreeSpaceSig:
 				// TODO what happens after a button is pressed
+
+				//TODO update its own map, check if the step was taken before
+				newLocation := PointStruct{
+					Point: Coordinate{
+						X: r.CurLocation.Point.X + r.NextStep.X,
+						Y: r.CurLocation.Point.Y + r.NextStep.Y,
+					},
+					PointKind:     true,
+					TraversedTime: time.Now().Unix(),
+					Traversed:     true,
+				}
+
+				exist, index := CheckExist(newLocation, r.RMap.ExploredPath)
+
+				if exist {
+					oldcoor := &(r.RMap.ExploredPath[index])
+					oldcoor.Point.X = newLocation.Point.X
+					oldcoor.Point.Y = newLocation.Point.Y
+					oldcoor.TraversedTime = newLocation.TraversedTime
+					oldcoor.Traversed = newLocation.Traversed
+					oldcoor.PointKind = newLocation.PointKind
+				} else {
+					r.RMap.ExploredPath = append(r.RMap.ExploredPath, newLocation)
+				}
+				r.WalkSig <- true
 			case <-r.WallSig:
 				// TODO update when hitting a wall
-			default:
-				// TODO walk around randomly 1 unit
+			case <-r.WalkSig:
+				// TODO walk around randomly by 1 unit
+				randomNum := rand.Intn(3)
+				if randomNum == 0 {
+					// go north
+					r.NextStep = Coordinate{0.0, 1.0}
+				} else if randomNum == 1 {
+					// go east
+					r.NextStep = Coordinate{1.0, 0.0}
+				} else if randomNum == 2 {
+					// go south
+					r.NextStep = Coordinate{0.0, -1.0}
+				} else {
+					// go west
+					r.NextStep = Coordinate{-1.0, 0.0}
+				}
 			}
 
 		}
@@ -94,4 +138,13 @@ func InitRobot(rID uint, initMap Map) Robot {
 
 func (r *RobotStruct) SetCurrentLocation(location PointStruct) {
 	r.CurLocation = location
+}
+
+func CheckExist(coordinate PointStruct, cooArr []PointStruct) (bool, int) {
+	for i, point := range cooArr {
+		if point.Point.X == coordinate.Point.X && point.Point.Y == coordinate.Point.Y {
+			return true, i
+		}
+	}
+	return false, -1
 }
