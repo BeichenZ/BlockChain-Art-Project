1e6a19e48f6b59e1059117cda6c9536cf9961a02
QuantumSpark <jamespark1234@hotmail.com>
2018-03-10 18:07:17 -0800

    register RPC call

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
+import (
+	"net/rpc"
+	"./shared"
+	"net"
+	"log"
+)
+
 // TODO: Include golang GPIO
 
 func main() {
+	/// Need to change to different ip address. May to use a different library due to ad-hoc
+	resolvedIPAddress, error := net.ResolveTCPAddr("tcp", "localhost")
+	if error != nil {
+		log.Fatal("Unable to resolve IP Address", error)
+	}
 
+	listener, error := net.ListenTCP("tcp", resolvedIPAddress)
+
+	if error != nil {
+		log.Fatal("Unable to create a listner", error)
+	}
+
+	robotRPC := shared.RobotRPC{}
+	rpc.Register(robotRPC)
+	go rpc.Accept(listener)
 
 	for {
-
 		// wait for user input
 		// if button is pressed, break out of the loop
 		break
@@ -15,7 +35,7 @@ func main() {
 	for {
 		// asynchronously check for other robots
 		// if a robot is nearby, get IP address and make RPC call
-		break 
+		break
 
 	}
 
