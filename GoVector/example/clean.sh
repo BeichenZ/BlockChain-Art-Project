48b0e39dc1a83573758d2d2f58284fd996ba0517
Howard-Zhou <howard12345678987654321@hotmail.com>
2018-03-25 23:42:15 -0700

    Removed GoVector from project dir

--- GoVector/example/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git checkout ClientServer.go
-sudo -E go install ../
-GoVector -file=ClientServer.go -v
-#dovid -file=client-nodoc.go -c
-#dovid -file=client-nodoc.go -v
-gofmt -w=true ClientServer.go
