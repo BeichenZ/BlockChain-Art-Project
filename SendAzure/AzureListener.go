12d6d16e22c722ab0327eb62b7582c0729183f73
Howard-Zhou <howard12345678987654321@hotmail.com>
2018-03-30 22:45:26 -0700

    Added AzureListener

---
+++ SendAzure/AzureListener.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	port := os.Args[1]
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on port", port)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+	}
+}
