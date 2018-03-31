6fcf3426ca0842460a32c4044bf59d4e94e9e991
QuantumSpark <jamespark1234@hotmail.com>
2018-03-30 19:27:02 -0700

    Switch between networks

---
+++ SendAzure/LocalListener.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"../shared"
+	"os/exec"
+
+	"os"
+	"fmt"
+	"net"
+	"bytes"
+	"encoding/gob"
+)
+
+
+type MapRPC struct {
+	latestMergedMap shared.Map
+}
+
+
+func DecodeMap(sentMap []byte) shared.Map  {
+
+	//robotLogContent, _ := ioutil.ReadFile("./" + r.Logname)
+	buf := bytes.NewBuffer(sentMap)
+
+	var decodedMap shared.Map
+
+	decoder := gob.NewDecoder(buf)
+	err := decoder.Decode(&decodedMap)
+	if err != nil {
+		panic(err)
+	}
+
+	return decodedMap
+}
+
+func handleRequest(conn net.Conn) {
+
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	lenInfo, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	newMap := DecodeMap(buf[:lenInfo])
+	fmt.Println("Successfully decoded info")
+	fmt.Println(newMap.ExploredPath)
+	fmt.Println(newMap.FrameOfRef)
+
+	// Turn off Wi-Fi interface
+	output, err := exec.Command("networksetup", "-setairportpower", "en0", "off").CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Println(string(output))
+
+	// Switch to UBC secure
+	output, err = exec.Command("networksetup", "-setairportpower", "en0", "on").CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Println(string(output))
+
+	//Wait until connected to Internet
+	WaitingForInternet:
+		for {
+			_, err := net.Dial("tcp", "Azure.com:8080")
+			if err != nil {
+				continue
+			} else {
+				break WaitingForInternet
+			}
+		}
+
+	//Send Map to Azure
+
+
+
+	//Change back to Ad-hoc
+	output, err = exec.Command("networksetup", "-setairportnetwork", "en0", "PiAdHocNetwork").CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Println(string(output))
+	
+	os.Exit(0)
+
+}
+
+
+func main() {
+	l, err := net.Listen("tcp", "169.254.82.142:8081")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on 169.254.82.142:8081")
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		handleRequest(conn)
+	}
+}
+
+
+
+
+/*
+networksetup -setairportnetwork en0 PiAdHocNetwork
+networksetup -setairportpower en0 off
+networksetup -setairportpower en0 on
+*/
