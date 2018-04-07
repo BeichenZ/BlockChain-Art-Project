05548bb3b8476a0b83a784384b51d69c1e9e58d8
Howard-Zhou <howard12345678987654321@hotmail.com>
2018-04-06 22:26:40 -0700

    I have miles to go before I sleep

--- shared/robot.go
+++ shared/robot.go
@@ -1062,61 +1062,6 @@ func InitRobot(rID int, initMap Map, ic Coordinate, logger *govec.GoLog, robotIP
 	return &newRobot
 }
 
-//func (r *RobotStruct) EncodeRobotLogInfo(robotLog RobotLog) string {
-//	buf := new(bytes.Buffer)
-//	encoder := gob.NewEncoder(buf)
-//	err := encoder.Encode(robotLog)
-//	if err != nil {
-//		panic(err)
-//	}
-//	output := string(buf.Bytes())
-//	return output
-//	// fmt.Println(buf.Bytes())
-//}
-//
-//func (r *RobotStruct) ReadFromLog() {
-//	robotLogContent, _ := ioutil.ReadFile("./" + r.Logname)
-//	buf := bytes.NewBuffer(robotLogContent)
-//
-//	var decodedRobotLog RobotLog
-//
-//	decoder := gob.NewDecoder(buf)
-//	err := decoder.Decode(&decodedRobotLog)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	r.RMap = decodedRobotLog.RMap
-//	r.CurLocation = decodedRobotLog.CurLocation
-//	r.CurrTask = decodedRobotLog.CurrTask
-//	fmt.Println(decodedRobotLog.RMap)
-//	fmt.Println(decodedRobotLog.CurLocation)
-//	fmt.Println(decodedRobotLog.CurrTask)
-//	fmt.Println("finshed loading from log")
-//}
-//
-//func (r *RobotStruct) CreateLog() (*os.File, error) {
-//	file, err := os.Create("./" + r.Logname)
-//	if err != nil {
-//		fmt.Println("error creating robot log")
-//	}
-//	return file, err
-//}
-//
-//func (r *RobotStruct) ProduceLogInfo() RobotLog {
-//	robotLog := RobotLog{
-//		CurrTask:    r.CurrTask,
-//		RMap:        r.RMap,
-//		CurLocation: r.CurLocation,
-//	}
-//	return robotLog
-//}
-//
-//func (r *RobotStruct) LocateLog() (*os.File, error) {
-//	file, err := os.Open(r.Logname)
-//	return file, err
-//}
-
 func (r *RobotStruct) UpdateStateForNewJourney() {
 
 	r.RobotNeighbours.Lock()
@@ -1258,12 +1203,3 @@ func (r *RobotStruct) MonitorFrontObsButtonOnPin(frontObsButton bgpio.Pin) {
 	}
 
 }
-
-// func (r *RobotStruct) MonitorLeftObstacleButton() {
-// 	for {
-// 		select {
-// 		case:
-//
-// 		}
-// 	}
-// }
